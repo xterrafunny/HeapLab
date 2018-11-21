393c7c766bcab3c78b2ca21e81f877a98dcfeec2
xterrafunny <gaibov.dr@phystech.edu>
2018-11-21 22:09:39 +0300

    Initial commit for Binomial Heap

---
+++ src/BinomialHeap/main.cpp
@@ -0,0 +1,6 @@
+#include "BinomialHeap.h"
+#include <iostream>
+using namespace std;
+int main() {
+  cout << "test";
+}
---
+++ test/BinomialHeap/test.cpp
@@ -0,0 +1,7 @@
+#define BOOST_TEST_MAIN
+#define BOOST_TEST_MODULE testBinomialHeap
+
+#include "BinomialHeap.h"
+#include <gtest/gtest.h>
+#include <random>
+#include <algorithm>
